a60c2f2e16851249eeb4491d8eb346d14b38cce7
Fedyaeva_Anna <aakorneva@gmail.com>
2024-02-14 16:36:59 +0300

    sprint1 - верстка основных компонентов и страниц

---
+++ src/mocks/messages.js
@@ -0,0 +1,17 @@
+export default {
+  name: "Друг",
+  avatar: "/avatar-friend.svg",
+  messages: [
+    {
+      time: "12:20",
+      text: "Нам предстоит трудный путь",
+      isMyMessage: false,
+    },
+    {
+      time: "12:40",
+      text: "Каким бы тяжелым ни был наш путь - вместе мы справимся",
+      isMyMessage: true,
+    },
+  ],
+};
+
